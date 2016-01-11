2ce4f112739190935b7acd9cf3752b97af627c11
Erich Duda <eduda@redhat.com>
2016-01-11 14:40:01 +0100

    Added -showsuccess option

--- src/main/groovy/com/redhat/tools/mavenlogparser/main.groovy
+++ src/main/groovy/com/redhat/tools/mavenlogparser/main.groovy
@@ -6,6 +6,7 @@ import static org.fusesource.jansi.Ansi.Color.*;
 
 def cli = new CliBuilder(usage: 'maven-log-parser [options] logfile')
 cli.errlines(args:1, argName:'number', 'number of error lines which will be printed for each test')
+cli.showsuccess(args:0, 'flag which defined whether you want to see success tests')
 cli.knownissues(args:1, argName:'file', 'file, which contains list of known failed tests. Each test is described by its fully Class name. For example: package.Test.testMethod')
 cli.help('print this help')
 def options = cli.parse(args)
@@ -22,6 +23,7 @@ if (!options.arguments().size()) {
 }
 
 def errlines = options.getProperty('errlines')?options.getProperty('errlines').toInteger():3
+def showsuccess = options.getProperty('showsuccess')?true:false
 def knownissues = options.getProperty('knownissues') ?
         new FileInputStream(options.getProperty('knownissues')).readLines().toSet()
         : new HashSet<String>()
@@ -32,15 +34,17 @@ def testsResult = Parser.parse(logfile)
 AnsiConsole.systemInstall();
 
 testsResult.each {
-    print ansi().fg(CYAN).a("TEST ").reset()
-
     if (it.errors.size() && knownissues.contains(it.name)) {
+        print ansi().fg(CYAN).a("TEST ").reset()
         println ansi().fg(YELLOW).a("${it.name} <<< KNOWN ISSUE").reset()
     } else if (it.errors.size()) {
+        print ansi().fg(CYAN).a("TEST ").reset()
         println ansi().fg(MAGENTA).a("${it.name} <<< ERROR").reset()
     } else if (!it.errors.size() && !it.stop) {
+        print ansi().fg(CYAN).a("TEST ").reset()
         println ansi().fg(MAGENTA).a("${it.name} <<< TIMEDOUT").reset()
-    } else {
+    } else if (showsuccess) {
+        print ansi().fg(CYAN).a("TEST ").reset()
         println ansi().fg(GREEN).a("${it.name} <<< SUCCESS").reset()
     }
     it.errors.each {
